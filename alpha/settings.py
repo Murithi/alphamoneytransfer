5640de48ae571adcd13a52d841219f8ce14cf518
Murithi <murithier.2006@gmail.com>
2015-10-21 10:46:41 +0300

    Edit db to ad engine

--- alpha/settings.py
+++ alpha/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'alpha.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         #     'NAME': 'beyonic',                      # Or path to database file if using sqlite3.
         #     # The following settings are not used with sqlite3:
         #     'USER': 'postgres',
