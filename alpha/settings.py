aea9a2bec7499505543badf3f84128cb7eb1cdff
Murithi <murithier.2006@gmail.com>
2015-10-21 10:49:36 +0300

    Edit db to add name value

--- alpha/settings.py
+++ alpha/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'alpha.wsgi.application'
 DATABASES = {
     'default': {
          'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        #     'NAME': 'beyonic',                      # Or path to database file if using sqlite3.
+             'NAME': 'beyonic',                      # Or path to database file if using sqlite3.
         #     # The following settings are not used with sqlite3:
         #     'USER': 'postgres',
         #     'PASSWORD': '',
