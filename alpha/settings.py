88dce732487d0690d7315a70ed9b41e946152ef0
Murithi <murithier.2006@gmail.com>
2015-10-21 10:39:30 +0300

    Edit db comments

--- alpha/settings.py
+++ alpha/settings.py
@@ -78,15 +78,15 @@ WSGI_APPLICATION = 'alpha.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     # 'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-    #     #     'NAME': 'beyonic',                      # Or path to database file if using sqlite3.
-    #     #     # The following settings are not used with sqlite3:
-    #     #     'USER': 'postgres',
-    #     #     'PASSWORD': '',
-    #     #     'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
-    #     #     'PORT': '5432',                      # Set to empty string for default.
-    #  }
+    'default': {
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        #     'NAME': 'beyonic',                      # Or path to database file if using sqlite3.
+        #     # The following settings are not used with sqlite3:
+        #     'USER': 'postgres',
+        #     'PASSWORD': '',
+        #     'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+        #     'PORT': '5432',                      # Set to empty string for default.
+     }
 }
 
 
